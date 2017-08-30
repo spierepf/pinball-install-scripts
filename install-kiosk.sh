98b2ed3db5b83f7f5315690b94d2387f3e2957bb
Peter-Frank <spierepf@hotmail.com>
2017-08-30 08:29:33 -0300

    Refactored /opt/kiosk.sh

--- install-kiosk.sh
+++ install-kiosk.sh
@@ -9,20 +9,44 @@ sudo update-initramfs -u
 
 sudo usermod -a -G audio $USER
 
-cat << EOF | sudo tee /opt/kiosk.sh
+sudo mkdir -p /opt/kiosk
+
+################################################################################
+
+cat << 'EOF' | sudo tee /opt/kiosk/main.sh
+#!/bin/bash
+
+/opt/kiosk/setup.sh
+
+while true; do
+  /opt/kiosk/loop.sh
+done
+EOF
+sudo chmod +x /opt/kiosk/main.sh
+
+################################################################################
+
+cat << 'EOF' | sudo tee /opt/kiosk/setup.sh
 #!/bin/bash
 
 xset -dpms
 xset s off
 openbox-session &
 start-pulseaudio-x11
-
-while true; do
-  rm -rf ~/.{config,cache}/google-chrome/
-  google-chrome --kiosk --no-first-run  'http://thepcspy.com'
-done
 EOF
-sudo chmod +x /opt/kiosk.sh
+sudo chmod +x /opt/kiosk/setup.sh
+
+################################################################################
+
+cat << 'EOF' | sudo tee /opt/kiosk/loop.sh
+#!/bin/bash
+
+rm -rf ~/.{config,cache}/google-chrome/
+google-chrome --kiosk --no-first-run  'http://thepcspy.com'
+EOF
+sudo chmod +x /opt/kiosk/loop.sh
+
+################################################################################
 
 cat << EOF | sudo tee /etc/init/kiosk.conf
 start on (filesystem and stopped udevtrigger)
@@ -33,8 +57,10 @@ emits starting-x
 
 respawn
 
-exec sudo -u $USER startx /etc/X11/Xsession /opt/kiosk.sh --
+exec sudo -u $USER startx /etc/X11/Xsession /opt/kiosk/main.sh --
 EOF
+
+################################################################################
 
 cat << EOF | sudo tee /etc/X11/Xwrapper.config
 allowed_users=anybody
