0d266e021bb7d8e63ddfebf66748e269b8b7372f
Peter-Frank <spierepf@hotmail.com>
2017-09-06 09:56:35 -0300

    Updated install-network.sh

--- install-network.sh
+++ install-network.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mv /etc/network/interfaces /etc/network/interfaces.orig
-head -n4 /etc/network/interfaces.orig > /etc/network/interfaces
-head -n8 /etc/network/interfaces.orig | tail -n3 > /etc/network/interfaces.d/lo
-tail -n3 /etc/network/interfaces.orig > /etc/network/interfaces.d/ens3
+if [ ! -f /etc/network/interfaces.orig ]
+then
+    sudo mv /etc/network/interfaces /etc/network/interfaces.orig
+fi
 
-sed -i s/auto/allow-hotplug/ /etc/network/interfaces.d/ens3
+head -n4 /etc/network/interfaces.orig | sudo tee /etc/network/interfaces
+head -n8 /etc/network/interfaces.orig | tail -n3 | sudo tee /etc/network/interfaces.d/lo
+tail -n3 /etc/network/interfaces.orig | sudo tee /etc/network/interfaces.d/ens3
+
+sudo sed -i s/auto/allow-hotplug/ /etc/network/interfaces.d/ens3
