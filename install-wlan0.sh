506734241e0f3258d8edc3c5084c95748d7b90a1
Peter-Frank Spierenburg <spierepf@hotmail.com>
2017-09-09 18:37:22 -0300

    Update install-wlan0.sh

--- install-wlan0.sh
+++ install-wlan0.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cat <<EOF | sudo tee /etc/network/interfaces.d/wlan0
-allow-hotplug wlan0
-iface wlan0 inet dhcp
+DEVICE=`ifconfig -a | grep ^wlx | cut -d' ' -f1`
+
+cat <<EOF | sudo tee /etc/network/interfaces.d/$DEVICE
+allow-hotplug $DEVICE
+iface $DEVICE inet dhcp
 wpa-ssid @ssid@
 wpa-psk @psk@
 EOF
 
-sudo nano /etc/network/interfaces.d/wlan0
+sudo nano /etc/network/interfaces.d/$DEVICE
