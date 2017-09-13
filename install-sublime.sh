c596f92b9abd5e8ce84e78080d52b0f40ad3c819
Peter-Frank <spierepf@hotmail.com>
2017-09-13 19:44:46 -0300

    Updated install-sublime.sh

--- install-sublime.sh
+++ install-sublime.sh
@@ -2,5 +2,5 @@
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt-get update
-apt-get install sublime-text
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends sublime-text
